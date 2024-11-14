2591c0f377a6c3db683e027f633eaf54e681aabe
Michael Hinkelmann <dfile.kassel@gmail.com>
2024-11-14 15:45:05 +0100

    Changes to be committed: 	modified:   index.php 	modified:   style.css

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
 get_header();
-if (have_posts()) {
-	while  (have_posts()) {
-	the_post();
+if (have_posts()): while (have_posts()): the_post();
 ?>
-	<h1><?php the_title(); ?></h1>
-	<?php the_content(); ?>
+	<h1><?=the_title(); ?></h1>
+	<?=the_content(); ?>
 <?php
-	}
-}
+endwhile; endif;
 get_footer();
 ?>
